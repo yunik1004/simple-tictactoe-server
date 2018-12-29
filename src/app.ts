3a2fb9541503b59c85a1d4d02e45ace3475e7138
yunik1004 <inkyuhak@gmail.com>
2018-12-29 17:27:05 +0900

    Naming change

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,15 @@ import http from 'http'
 import Koa from 'koa'
 import socketIO from 'socket.io'
 import { PORT } from './constants'
-import { createSocket } from './socketio/socket'
+import { createSocketEvent } from './socketio/socket'
 
 const app = new Koa()
 
 const server = http.createServer(app.callback())
 const io = socketIO(server)
 
-createSocket(io)
+createSocketEvent(io)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('Server is listening to port ' + PORT)
 })
--- src/socketio/socket.ts
+++ src/socketio/socket.ts
@@ -1,7 +1,11 @@
 import socketIO from 'socket.io'
 
-export function createSocket (io: socketIO.Server) {
+export function createSocketEvent (io: socketIO.Server) {
   io.on('connection', (socket: socketIO.Socket) => {
-    console.log('Connected')
+    console.log('Client connected')
+
+    socket.on('disconnect', () => {
+      console.log('Client disconnected')
+    })
   })
 }
